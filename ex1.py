b878ce7a9f96f26a3bc3c4e136099962273d25a9
monarkha <mikita.monarkha@yandex.by>
2021-01-27 21:28:22 +0300

    trying recursion

--- ex1.py
+++ ex1.py
@@ -1,7 +1,12 @@
 def summ(*nums):
-    sum = 0
-    for i in nums:
-        sum += i * nums.index(i)
-    return sum
+    if len(nums) == 0:
+        return
+    v=list(nums)
 
+    #sum = 0
+    #for i in nums:
+     #   sum += i * nums.index(i)
+    #return sum
+    c=v.index((v[-1]))*v.pop() + summ(v)
+    return c
 print(summ(1,2,3,4))
