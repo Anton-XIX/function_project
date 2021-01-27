74ac70cdc9b80568fc71118d9c9a4d0cd07ad4ed
monarkha <mikita.monarkha@yandex.by>
2021-01-27 20:04:22 +0300

    created file z2

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print('Hello World')
+def my_func(a, b):
+    summ = a + b
+    print(f'{a} + {b} = {summ}')
+    return summ, a, b
+
+
+z = my_func(4, 5)
+print(z)
---
+++ z2.py
@@ -0,0 +1,8 @@
+def print_hello(name):
+    print(f'Hello, {name}')
+    return 0
+
+
+names = ['Anton', 'Igor', 'Petr', 'Oleg', 'Nastya']
+for i in names:
+    print_hello(i)
