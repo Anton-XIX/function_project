92e97cc6ed1305e18cd84d5e9bc853f72fe90626
monarkha <mikita.monarkha@yandex.by>
2021-01-27 20:45:04 +0300

    using recursion

--- z3.py
+++ z3.py
@@ -1,8 +1,11 @@
 def factorial(number):
-    fact = 1
-    for i in range(1,number+1):
-        fact *= i
-    return fact
+    if number ==0:
+        return 1
+    return number * factorial(number-1)
+    #fact = 1
+    #for i in range(1, number+1):
+     #   fact *= i
+    #return fact
 
 
 print(factorial(int(input())))
